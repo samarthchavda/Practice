ab34fb4e0bd7b0721b66bddb40b48226d4082233
Samarth Chavda <samarthchavda@example.com>
2026-05-05 18:50:20 +0530

    modifed a order page and total

--- mini_sales_order_system/models/order.py
+++ mini_sales_order_system/models/order.py
@@ -4,7 +4,7 @@ class Order(models.Model):
     _name = 'sales.order'
     _description = 'Sales Order'
 
-    order_number = fields.Char(string="Order Number", required=True, copy=False, readonly=True, default="New")
+    order_number = fields.Char(string="Order Number", required=True, copy=False, readonly=True)
     date = fields.Date(string="date")
 
     order_line_ids = fields.One2many(
@@ -15,11 +15,15 @@ class Order(models.Model):
 
     total_amount = fields.Float(
         string="Total Amount",
-        compute="_compute_total_amount"
+        compute="_compute_total_amount",
+        readonly=True
     )
 
     @api.depends('order_line_ids.subtotal')
     def _compute_total_amount(self):
         for order in self:
             order.total_amount = sum(order.order_line_ids.mapped('subtotal'))
-
+            # with gst add total prodct
+            # subtotal = sum(order.order_line_ids.mapped('subtotal'))
+            # gst = subtotal * 18 / 100
+            # order.total_amount = subtotal + gst
